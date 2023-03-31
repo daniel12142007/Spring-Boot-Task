d1b6be80ff94cd9aad0827194cbec429b689c651
daniel12142007 <danijelahatzanov@gmail.com>
2023-03-31 20:48:18 +0600

    Initial commit

--- src/main/java/com/example/startedspringbootaplication/repository/StudentRepository.java
+++ src/main/java/com/example/startedspringbootaplication/repository/StudentRepository.java
@@ -2,6 +2,15 @@ package com.example.startedspringbootaplication.repository;
 
 import com.example.startedspringbootaplication.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String name);
+
+    @Query("select case when count(u)>0 then true else false end from Student u where u.email like :email")
+    boolean existsByEmail(@Param(value = "email") String email);
 }
