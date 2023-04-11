d695a5adbd2e4265e09187c8e35be132a85a132a
daniel12142007 <danijelahatzanov@gmail.com>
2023-04-11 10:41:49 +0600

    Initial commit

--- src/main/java/com/example/startedspringbootaplication/service/ServiceVersionCompany.java
+++ src/main/java/com/example/startedspringbootaplication/service/ServiceVersionCompany.java
@@ -13,6 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+
+
+
 import java.util.List;
 
 @Service
